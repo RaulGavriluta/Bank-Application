ba6ce7adfd7304e6517c95f99f7d840440bda35d
Raul Gavriluta <raul.gavriluta7@gmail.com>
2025-08-15 19:21:53 +0300

    first unit tests

---
+++ BankApp.nUnitTests/BankUserControllerTests/DeleteBankUserTests.cs
@@ -0,0 +1,48 @@
+using Moq;
+using NUnit.Framework;
+using BankApp.Data;
+using BankApp.Interfaces;
+using BankApp.Controllers;
+using Microsoft.AspNetCore.Mvc;
+namespace BankApp.nUnitTests.BankUserControllerTests
+{
+    public class DeleteBankUserTests
+    {
+        private Mock<IBankUserDataOps> _bankUserDataOpsMock;
+        private BankUserController _bankUsersController;
+
+        [SetUp]
+        public void Setup()
+        {
+            _bankUserDataOpsMock = new Mock<IBankUserDataOps>();
+            _bankUsersController = new BankUserController(_bankUserDataOpsMock.Object);
+        }
+
+        [Test]
+        public async Task DeleteBankUserAsync_UserExists_ReturnsNoContent()
+        {
+            //Arrange
+            _bankUserDataOpsMock.Setup(s => s.DeleteBankUserAsync(It.IsAny<int>()))
+                .Returns(Task.CompletedTask);
+            //Act
+            var result = await _bankUsersController.DeleteBankUser(1);
+            //Assert
+            Assert.IsInstanceOf<NoContentResult>(result);   
+        }
+
+        [Test]
+        public async Task DeleteBankUserAsync_NotExistingUser_ReturnsNotFound()
+        {
+            //Arrange
+            _bankUserDataOpsMock.Setup(s => s.DeleteBankUserAsync(It.IsAny<int>())).ThrowsAsync(new ArgumentException("User with id 1 does not exist."));
+
+            //Act
+            var result = await _bankUsersController.DeleteBankUser(1);
+            var message = result as NotFoundObjectResult;
+            //Assert
+            Assert.IsInstanceOf<NotFoundObjectResult>(result);
+            Assert.AreEqual("User with id 1 does not exist.", message.Value);
+
+        }
+    }
+}
---
+++ BankApp.nUnitTests/BankUserControllerTests/GetAllBankUsersTests.cs
@@ -0,0 +1,44 @@
+﻿using BankApp.Controllers;
+using BankApp.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using System.Threading.Tasks;
+using Moq;
+using BankApp.DTO;
+using BankApp.Models;
+
+namespace BankApp.nUnitTests;
+
+public class GetAllBankUsersTests
+{
+    private Mock<IBankUserDataOps> _bankUserDataOpsMock;
+    private BankUserController _bankUserController;
+
+    [SetUp]
+
+    public void Setup()
+    {
+        _bankUserDataOpsMock = new Mock<IBankUserDataOps>();
+        _bankUserController = new BankUserController(_bankUserDataOpsMock.Object);
+    }
+
+    [Test]
+    public async Task GetAllBankUsers_ReturnsOk()
+    {
+        //Arrange
+        var users = new BankUserDTO[]
+        {
+            new BankUserDTO { 
+                BankUserId = 1, 
+                Name ="John Doe", 
+                Email = "johndoe@example.com", 
+                Password = "parola123", 
+                Phone = "0722222222" }
+        };
+        _bankUserDataOpsMock.Setup(s => s.GetBankUsersAsync()).ReturnsAsync(users);
+        //Act
+        var result = await _bankUserController.GetAllBankUsers();
+        //Assert
+        Assert.IsInstanceOf<OkObjectResult>(result);
+    }
+}
